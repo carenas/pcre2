3d99a24547554517196582d00beaba807830c50d
Carlo Marcelo Arenas Belón <carenas@gmail.com>
2023-01-27 01:03:29 -0800

    chkdint: use prefer __builtin_mul_overflow() if available
    Unlike our check, works also for negative integers

--- src/pcre2_chkdint.c
+++ src/pcre2_chkdint.c
@@ -54,7 +54,7 @@ POSSIBILITY OF SUCH DAMAGE.
 /*
 Arguments:
   r         A pointer to PCRE2_SIZE to store the answer
-  a, b      Two positive integers
+  a, b      Two integers
 
 Returns:    Bool indicating if the operation overflows
 
@@ -65,6 +65,13 @@ otherwise double. */
 BOOL
 PRIV(ckd_smul)(PCRE2_SIZE *r, int a, int b)
 {
+#ifdef HAVE_BUILTIN_MUL_OVERFLOW
+PCRE2_SIZE m;
+
+if (__builtin_mul_overflow(a, b, &m)) return TRUE;
+
+*r = m;
+#else
 INT64_OR_DOUBLE m;
 
 #ifndef NDEBUG
@@ -75,6 +82,8 @@ m = (INT64_OR_DOUBLE)a * (INT64_OR_DOUBLE)b;
 if (sizeof(m) == sizeof(int) && m > (INT64_OR_DOUBLE)PCRE2_SIZE_MAX) return TRUE;
 
 *r = (PCRE2_SIZE)m;
+#endif
+
 return FALSE;
 }
 
